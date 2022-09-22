cce72b0b6235f7debc3627a9ce71720e3ad00554
Chrsteffen <Chrsteffen1@gmail.com>
2022-09-22 19:43:42 -0400

    add update function

--- controllers/skills.js
+++ controllers/skills.js
@@ -53,10 +53,38 @@ function deleteSkill(req,res) {
   })
 }
 
+function edit(req, res) {
+
+  Skills.findById(req.params.id)
+  .then(skill => {
+    res.render('skills/edit', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/todos')
+  })
+}
+
+function update(req,res) {
+  req.body.learned = !!req.body.learned
+  Skills.findByIdAndUpdate(req.params.id,req.body, {new:true})
+  .then(skill => {
+    res.redirect(`/skills/${skill._id}`)
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/todos')
+  })
+}
+
 export {
   index,
   newSkill as new,
   create,
   show,
   deleteSkill as delete,
+  update,
+  edit,
 }
